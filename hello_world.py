484d00ca688ace6d4031582eb5a80e3ad297d417
Aditya <aditya.fly@gmail.com>
2015-06-09 21:23:40 -0700

    Sample hello_world.py program to test github account

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,1 @@
-import sys
-def main():
-  if len(sys.argv) >= 2:
-    name = sys.argv[1]
-  else:
-    name = 'World'
-  print 'Hello', name
-  if __name__ == '__main__':
-  main()
+print "Hello World"
