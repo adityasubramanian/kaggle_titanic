6a4d6491b54aad29453abd82221f1464e30aeb02
Aditya <aditya.fly@gmail.com>
2015-06-20 21:06:59 -0700

    "up to date"

--- random_forsts.r
+++ random_forsts.r
@@ -1,20 +1,29 @@
 #install.packages("Amelia")
 #install.packageS("corrorgram")
 #install.packages("plyr")
+#install.packages("Hmisc")
+install.packages("plyr")
+#install.packages("ggplot2")
+#install.packages("scales")
+install.packages("Hmisc")
+
+library(scales)
+library(ggplot2)
 library(Amelia) 
 library(corrgram)
-
+library(Hmisc) 
 require(Amelia) 
 require(corrgram)
+require(Hmisc)
 
 readData <- function(path.name, file.name, column.types, missing.types) {
   read.csv( url( paste(path.name, file.name, sep="") ), 
             colClasses=column.types,
             na.strings=missing.types )
 }
-Titanic.path <- "https://github.com/adityasubramanian/kaggle_titanic/tree/master/kaggle_data"
+Titanic.path <- "https://raw.github.com/wehrley/Kaggle_Titanic/master/"
 train <- "train.csv"
-test  <- "test.csv"
+test  <- "test.csv" 
 missing.types <- c("NA", "")
 train.column.types <- c('integer',   # PassengerId
                         'factor',    # Survived 
@@ -32,11 +41,11 @@ train.column.types <- c('integer',   # PassengerId
 test.column.types <- train.column.types[-2] 
 train.raw <- readData(Titanic.path, train.data.file, 
                       train.column.types, missing.types)
-df.train <- train.raw
+df.train <- read.csv("train.csv", header = TRUE) 
 
 test.raw <- readData(Titanic.path, test.data.file, 
                      test.column.types, missing.types)
-df.infer <- test.raw   
+df.infer <- read.csv("test.csv")
 
 missmap(df.train, main="Titanic Training Data - Missings Map", 
         col=c("yellow", "black"), legend=FALSE)
@@ -84,3 +93,19 @@ corrgram.vars <- c("Survived", "Pclass", "Sex", "Age",
 corrgram(corrgram.data[,corrgram.vars], order=FALSE, 
          lower.panel=panel.ellipse, upper.panel=panel.pie, 
          text.panel=panel.txt, main="Titanic Training Data")
+summary(df.train$Age)
+names(df.train)
+head(df.train$Name, n = 10L)
+getTitle <- function(data) {
+  title.dot.start <- regexpr("\\,[A-Z ]{1,20}\\.", data$Name, TRUE)
+  title.comma.end <- title.dot.start + attr(title.dot.start, "match.length")-1
+  data$Title <- substr(data$Name, title.dot.start+2, title.comma.end-1)
+  return (data$Title)
+}   
+df.train$Title <- getTitle(df.train)
+unique(df.train$Title)
+options(digits=2)
+require(Hmisc)
+
+bystats(df.train$Age, df.train$Title, 
+        fun=function(x)c(Mean=mean(x),Median=median(x)))
